795c3c9f3e426e280acf2563567121823d558d5a
Tetiana Zykova <tatiana.nv96@gmail.com>
2022-12-23 16:51:29 +0200

    remove env file

--- src/servises/api.js
+++ src/servises/api.js
@@ -1,5 +1,5 @@
 const BASE_URL = 'https://pixabay.com/api';
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = '24468918-f1629215ca3337ba51b4044a7';
 
 const fetchImages = (serchQuery, page) => {
   const url = `${BASE_URL}/?q=${serchQuery}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=20
